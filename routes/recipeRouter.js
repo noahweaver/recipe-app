43e67511ffed41d7fb0eaf5b1ddf52864c70b22d
Noah Weaver <noahweaver@aol.com>
2021-12-13 11:13:58 -0600

    getone and put

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -2,6 +2,9 @@ const express = require("express")
 const recipeRouter = express.Router()
 const Recipe = require('../models/recipe')
 
+
+//Routes
+//("/") Routes
 recipeRouter.route("/")
     .get((req, res, next) => {
         Recipe.find((err, bounties) => {
@@ -23,6 +26,30 @@ recipeRouter.route("/")
         })
     })
 
+//("/:recipeId") Routes
+recipeRouter.route("/:recipeId")
+    .get((req, res, next) => {
+        Recipe.find({_id: req.params.recipeId},
+            (err, recipe) => {
+            if(err){
+                res.status(500)
+                return next(err)
+            }
+            return res.status(200).send(recipe)
+        })
+    })
+    .put((req, res, next) => {
+        Recipe.findOneAndUpdate({_id: req.params.recipeId},
+            req.body,
+            {new: true},
+            (err, updatedRecipe) => {
+                if(err){
+                    res.status(500)
+                    return next(err)
+                }
+                return res.status(201).send(updatedRecipe)
+            })
+    })
 
     module.exports = recipeRouter
 
