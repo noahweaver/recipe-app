0843dabb9f4bd550d82c9bf0a68f9935a206206c
Noah Weaver <noahweaver@aol.com>
2021-12-14 10:35:28 -0600

    routed last two links

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
+import Search from './components/Search'
+import Submit from './components/Submit'
 
 function App() {
     return (
@@ -10,7 +12,10 @@ function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />}/>
                     <Route path="home" element={<Home />}/>
+                    <Route path="search" element={<Search />}/>
+                    <Route path="submit-recipe" element={<Submit />}/>
                 </Route>
+                {/* not found path */}
             </Routes>
         </>
     )
