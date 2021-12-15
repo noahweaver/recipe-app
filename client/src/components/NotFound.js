4dc3cbc14d16bb674a6ac4a0542d65c4c95417a8
Noah Weaver <noahweaver@aol.com>
2021-12-14 23:45:48 -0600

    created notfound component and route

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+
+function NotFound() {
+    return (
+        <div>
+            <Navbar />
+            <h1>NOT FOUND</h1>
+        </div>
+    )
+}
+
+export default NotFound
