e2bf5c540652862b20ce56540b66a827755d39d5
Noah Weaver <noahweaver@aol.com>
2021-12-15 10:22:09 -0600

    included featured recipe on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import RecipeCard from './Recipecard'
+import React, {useContext} from 'react'
+import {Context} from '../Context'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 function Home(props) {
+
+    const {featuredRecipe} = useContext(Context)
+
     return (
         <div className="container-fluid d-flex flex-row justify-content-evenly p-5 mb-5">
-            <RecipeCard />
+
+                <Card style={{ width: '38%' }} className="text-center bg-dough text-almost-black mx-10p">
+                <Card.Header as='h1'>Featured Recipe</Card.Header>
+                <Card.Img className="mx-auto mt-3 rounded" variant="top" src={featuredRecipe.imgUrl} style={{ width: "200px" }}/>
+                <Card.Body>
+                    <Card.Title as="h2">{featuredRecipe.name}</Card.Title>
+                    <Card.Subtitle>{featuredRecipe.type}</Card.Subtitle>
+                    <Card.Text><b>Required Ingredients:</b> {featuredRecipe.ingredients}</Card.Text>
+                    <Card.Text><b>Directions:</b> {featuredRecipe.directions}</Card.Text>
+                    <Card.Text><b>Author:</b> <a href={featuredRecipe.author}>{featuredRecipe.author}</a></Card.Text>
+                    {/* Button to navigate to recipe dashboard */} 
+                    <Button type='button' className="btn btn-secondary">Go To Recipe Dashboard</Button>
+                </Card.Body>
+                </Card>
             
             <div className="w-35p pt-5">
                 <h1>HOME PAGE</h1>
-            <p>contentcontentcontentcontentcontent</p>
-            <p>contentcontentcontentcontentcontent</p>
-            <p>contentcontentcontentcontentcontent</p>
+                <p>contentcontentcontentcontentcontent</p>
+                <p>contentcontentcontentcontentcontent</p>
+                <p>contentcontentcontentcontentcontent</p>
             </div>
             
         </div>
