44e609c8b27a85ebcbce178bfb7a9eb9a0e96b6b
Noah Weaver <noahweaver@aol.com>
2021-12-14 23:44:49 -0600

    added routes, they might be wrong, need to review

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout'
 import Home from './components/Home'
 import Search from './components/Search'
 import Submit from './components/Submit'
+import NotFound from './components/NotFound'
 
 function App() {
     return (
@@ -12,10 +13,17 @@ function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />}/>
                     <Route path="home" element={<Home />}/>
-                    <Route path="search" element={<Search />}/>
-                    <Route path="submit-recipe" element={<Submit />}/>
+                    <Route path="search" element={<Search />}>
+                        <Route index element={<Search />}/>
+                        <Route path=":search" element={<Search />}/>
+                    </Route>
+                    <Route path="submit-recipe" element={<Submit />}>
+                        <Route index element={<Submit />}/>
+                        <Route path=":submitted" element={<Submit />}>
+                    </Route>
                 </Route>
-                {/* not found path */}
+                </Route>
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </>
     )
